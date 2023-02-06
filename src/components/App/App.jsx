ef69906e32b86187d28b70568347fd55e9f16c10
Николай Ковбасинский <kvbsnsk@gmail.com>
2023-02-06 22:53:06 +0200

    up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,29 @@
 import css from './app.module.css';
 import { Searchbar } from '../Searchbar/Searchbar';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div className={css.App}>
-      <Searchbar />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    search: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('this.state ', this.state);
+    console.log('prevState ', prevState);
+  }
+
+  handlSearch = value => {
+    this.setState({
+      search: value,
+    });
+  };
+
+  render() {
+    const { handlSearch } = this;
+    return (
+      <div className={css.App}>
+        <Searchbar onSubmit={handlSearch} />
+      </div>
+    );
+  }
+}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,21 +1,51 @@
 import css from './searchbar.module.css';
+import { Component } from 'react';
+import { CiSearch } from 'react-icons/ci';
 
-export function Searchbar() {
-  return (
-    <header className={css.Searchbar}>
-      <form className={css.SearchForm}>
-        <button type="submit" className={css.SearchFormButton}>
-          <span className={css.SearchFormButtonLabel}>Search</span>
-        </button>
+export class Searchbar extends Component {
+  state = {
+    search: '',
+  };
 
-        <input
-          className={css.SearchFormInput}
-          type="text"
-          autocomplete="off"
-          autofocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    </header>
-  );
+  handleChange = ({ target }) => {
+    // console.log(target.value);
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handlSubmit = e => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    onSubmit(this.state.search);
+    this.reset();
+  };
+  reset() {
+    this.setState({ search: '' });
+  }
+
+  render() {
+    const { search } = this.state;
+    const { handleChange, handlSubmit } = this;
+    return (
+      <header className={css.Searchbar}>
+        <form className={css.SearchForm} onSubmit={handlSubmit}>
+          <button type="submit" className={css.SearchFormButton}>
+            <CiSearch />
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            onChange={handleChange}
+            value={search}
+            name="search"
+            className={css.SearchFormInput}
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
 }
